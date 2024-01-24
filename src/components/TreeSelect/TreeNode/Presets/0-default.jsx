6b84672293a9a4679febc923423070bdc026a9c7
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-01-24 16:23:50 -0700

    Cleanup

--- src/components/TreeSelect/TreeNode/Presets/0-default.jsx
+++ src/components/TreeSelect/TreeNode/Presets/0-default.jsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import TreeNode from '../TreeNode';
 
 export default (
-  <TreeNode uxpId="treenode-1" title="testing1" />
+  <TreeNode uxpId="treenode-1" title="Tree node" />
 
 );
