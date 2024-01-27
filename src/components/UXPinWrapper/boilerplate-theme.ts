8a1b86464bb024fc07cc6612e3bac3ef9158d76f
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-01-27 11:28:53 -0700

    Theme test

--- src/components/UXPinWrapper/boilerplate-theme.ts
+++ src/components/UXPinWrapper/boilerplate-theme.ts
@@ -3,8 +3,8 @@ export const theme = {
   // hashed: false,
   token: {
     // Seed Token
-    colorPrimary: '#ff0000',
-    borderRadius: 2,
+    // colorPrimary: '#ff0000',
+    // borderRadius: 2,
 
     // // Alias Token
     // colorBgContainer: '#f6ffed',
