6ba0144f7d90d30c2efdf79bb0e18871aedf5742
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-08 14:01:01 -0700

    revert theme tests

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -4,10 +4,7 @@ import React from 'react';
 import defaultTheme from './boilerplate-theme';
 
 import { ConfigProvider } from 'antd';
-import './index.css'
-
-// import "antd/dist/reset.css";
-
+// import './index.css'
 
 
 export const ThemeContext = React.createContext({});
