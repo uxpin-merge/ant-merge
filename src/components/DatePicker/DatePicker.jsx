476f230d708e8121157b56b6bfb8fc698c5f478b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 10:37:45 -0700

    Update props

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,156 +1,154 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { DatePicker as ANTComponent } from "antd";
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { DatePicker as ANTComponent } from 'antd'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 
-dayjs.extend(customParseFormat);
+dayjs.extend(customParseFormat)
 /**
  * @uxpindocurl https://ant.design/components/DatePicker/
  * @uxpindescription TODO
  */
 
 const DatePicker = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent {...props} />
+  )
+}
 
 DatePicker.propTypes = {
-    /** If get focus when component mounted */
-    autoFocus: PropTypes.bool,
+  /** If get focus when component mounted */
+  autoFocus: PropTypes.bool,
 
-    /** Customize clear button */
-    allowClear: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.shape({ clearIcon: PropTypes.node })
-    ]),
+  /** Customize clear button */
+  allowClear: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({ clearIcon: PropTypes.node })]),
 
-    /** Trigger change when blur (e.g., datetime picker no need click confirm button) */
-    changeOnBlur: PropTypes.bool,
+  /** Trigger change when blur (e.g., datetime picker no need click confirm button) */
+  changeOnBlur: PropTypes.bool,
 
-    /** Custom rendering function for picker cells */
-    cellRender: PropTypes.func,
+  /** Custom rendering function for picker cells */
+  cellRender: PropTypes.func,
 
-    /** The picker className */
-    className: PropTypes.string,
+  /** The picker className */
+  className: PropTypes.string,
 
-    /** Custom rendering function for date cells (use cellRender instead for version >= 5.4.0) */
-    dateRender: PropTypes.func,
+  /** Custom rendering function for date cells (use cellRender instead for version >= 5.4.0) */
+  dateRender: PropTypes.func,
 
-    /** Determine whether the DatePicker is disabled */
-    disabled: PropTypes.bool,
+  /** Determine whether the DatePicker is disabled */
+  disabled: PropTypes.bool,
 
-    /** Specify the date that cannot be selected */
-    disabledDate: PropTypes.func,
+  /** Specify the date that cannot be selected */
+  disabledDate: PropTypes.func,
 
-    /** To set the date format, support multi-format matching when it's an array */
-    format: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.arrayOf(PropTypes.string)
-    ]),
+  /** To specify the time that cannot be selected	 */
+  disabledTime: PropTypes.bool,
 
-    /** To set the container of the floating layer */
-    getPopupContainer: PropTypes.func,
+  /** To set the date format, support multi-format matching when it's an array */
+  format: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
 
-    /** Set the readonly attribute of the input tag */
-    inputReadOnly: PropTypes.bool,
+  /** To set the container of the floating layer */
+  getPopupContainer: PropTypes.func,
 
-    /** Localization configuration */
-    locale: PropTypes.object,
+  /** Set the readonly attribute of the input tag */
+  inputReadOnly: PropTypes.bool,
 
-    /** The picker panel mode */
-    mode: PropTypes.oneOf(['time', 'date', 'month', 'year', 'decade']),
+  /** Localization configuration */
+  locale: PropTypes.object,
 
-    /** The custom next icon */
-    nextIcon: PropTypes.node,
+  /** The picker panel mode */
+  mode: PropTypes.oneOf(['time', 'date', 'month', 'year', 'decade']),
 
-    /** Callback function when the popup calendar is popped up or closed */
-    onOpenChange: PropTypes.func,
+  /** Enable multiple selection. Not support showTime	 */
+  multiple: PropTypes.bool,
 
-    /** Callback when click ok button */
-    onOk: PropTypes.func,
+  /** The custom next icon */
+  nextIcon: PropTypes.node,
 
-    /** Callback function for panel changing */
-    onPanelChange: PropTypes.func,
+  /** Callback function when the popup calendar is popped up or closed */
+  onOpenChange: PropTypes.func,
 
-    /** The open state of picker */
-    open: PropTypes.bool,
+  /** Callback when click ok button */
+  onOk: PropTypes.func,
 
-    /** Customize panel render */
-    panelRender: PropTypes.func,
+  /** Callback function for panel changing */
+  onPanelChange: PropTypes.func,
 
-    /** The placeholder of date input */
-    placeholder: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.arrayOf(PropTypes.string)
-    ]),
+  /** The open state of picker */
+  open: PropTypes.bool,
 
-    /** The position where the selection box pops up */
-    placement: PropTypes.oneOf(['bottomLeft', 'bottomRight', 'topLeft', 'topRight']),
+  /** Customize panel render */
+  panelRender: PropTypes.func,
 
-    /** The custom prev icon */
-    prevIcon: PropTypes.node,
+  /** The placeholder of date input */
+  placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
 
-    /** To customize the style of the popup calendar */
-    popupStyle: PropTypes.object,
+  /** The position where the selection box pops up */
+  placement: PropTypes.oneOf(['bottomLeft', 'bottomRight', 'topLeft', 'topRight']),
 
-    /** The preset ranges for quick selection */
-    presets: PropTypes.arrayOf(PropTypes.shape({
-        label: PropTypes.node,
-        value: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
-    })),
+  /** The custom prev icon */
+  prevIcon: PropTypes.node,
 
-    /** Set picker type */
-    picker: PropTypes.oneOf(['date', 'week', 'month', 'quarter', 'year']),
+  /** To customize the style of the popup calendar */
+  popupStyle: PropTypes.object,
 
-    /** Render extra footer in panel */
-    renderExtraFooter: PropTypes.func,
+  /** The preset ranges for quick selection */
+  presets: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.node,
+      value: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    })
+  ),
 
-    /** Whether to show 'Now' button on panel when showTime is set */
-    showNow: PropTypes.bool,
+  /** Set picker type */
+  picker: PropTypes.oneOf(['date', 'week', 'month', 'quarter', 'year']),
 
-    /** To provide an additional time selection */
-    showTime: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.object // TimePicker Options
-    ]),
+  /** Render extra footer in panel */
+  renderExtraFooter: PropTypes.node,
 
-    /** To set default time of selected date */
-    showTimeDefaultValue: PropTypes.object, // Assuming dayjs object, you can adjust the type accordingly 
+  /** Whether to show 'Now' button on panel when showTime is set */
+  showNow: PropTypes.bool,
 
-    /** Whether to show Today button */
-    showToday: PropTypes.bool,
+  /** To provide an additional time selection */
+  showTime: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.object, // TimePicker Options
+  ]),
 
-    /** To determine the size of the input box */
-    size: PropTypes.oneOf(['large', 'middle', 'small']),
+  /** To set default time of selected date */
+  showTimeDefaultValue: PropTypes.object, // Assuming dayjs object, you can adjust the type accordingly
 
-    /** Set validation status */
-    status: PropTypes.oneOf(['error', 'warning']),
+  /** Whether to show Today button */
+  showToday: PropTypes.bool,
 
-    /** To customize the style of the input box */
-    style: PropTypes.object,
+  /** To determine the size of the input box */
+  size: PropTypes.oneOf(['large', 'middle', 'small']),
 
-    /** The custom suffix icon */
-    suffixIcon: PropTypes.node,
+  /** Set validation status */
+  status: PropTypes.oneOf(['error', 'warning']),
 
-    /** The custom super next icon */
-    superNextIcon: PropTypes.node,
+  /** To customize the style of the input box */
+  style: PropTypes.object,
 
-    /** The custom super prev icon */
-    superPrevIcon: PropTypes.node,
+  /** The custom suffix icon */
+  suffixIcon: PropTypes.node,
 
-    /** Variants of picker */
-    variant: PropTypes.oneOf(['outlined', 'borderless', 'filled']),
+  /** The custom super next icon */
+  superNextIcon: PropTypes.node,
 
-    /** To set date */
-    value: PropTypes.object, // Assuming dayjs object, you can adjust the type accordingly 
-};
+  /** The custom super prev icon */
+  superPrevIcon: PropTypes.node,
 
-DatePicker.defaultProps = {};
+  /** Variants of picker */
+  variant: PropTypes.oneOf(['outlined', 'borderless', 'filled']),
 
-export default DatePicker;
+  /** To set date */
+  value: PropTypes.object, // Assuming dayjs object, you can adjust the type accordingly
+}
+
+DatePicker.defaultProps = {}
+
+export default DatePicker
