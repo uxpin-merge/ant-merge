6b45bba8319f3124e095f9925ae10e7fb75157a6
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 12:00:10 -0700

    Updated prop

--- src/components/Avatar/Group/Group.jsx
+++ src/components/Avatar/Group/Group.jsx
@@ -1,54 +1,44 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { Avatar as ANTComponent } from "antd";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Avatar as ANTComponent } from 'antd'
 
 /**
  * @uxpindocurl https://ant.design/components/Avatar/
  * @uxpindescription Avatars can be used to represent people or objects. It supports images, Icons, or letters.
  * @uxpinnamespace Avatar
-*/
+ */
 
 const Group = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent.Group {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent.Group {...props} />
+  )
+}
 
 Group.propTypes = {
-    /** The content of the component. */
-    children: PropTypes.node,
-    /** Max avatars to show */
-    maxCount: PropTypes.number,
+  /** The content of the component. */
+  children: PropTypes.node,
+  /** Max avatars to show */
+  maxCount: PropTypes.number,
 
-    /** The placement of excess avatar Popover ('top' or 'bottom') */
-    maxPopoverPlacement: PropTypes.oneOf(['top', 'bottom']),
+  /** The placement of excess avatar Popover ('top' or 'bottom') */
+  maxPopoverPlacement: PropTypes.oneOf(['top', 'bottom']),
 
-    /** Set the trigger of excess avatar Popover ('hover', 'focus', 'click') */
-    maxPopoverTrigger: PropTypes.oneOf(['hover', 'focus', 'click']), // Since 4.17.0
+  /** Set the trigger of excess avatar Popover ('hover', 'focus', 'click') */
+  maxPopoverTrigger: PropTypes.oneOf(['hover', 'focus', 'click']), // Since 4.17.0
 
-    /** The style of excess avatar */
-    maxStyle: PropTypes.object, // CSSProperties
+  /** The style of excess avatar */
+  maxStyle: PropTypes.object, // CSSProperties
 
-    /** The size of the avatar (number, 'large', 'small', 'default', or an object for responsive sizes) */
-    size: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.oneOf(['large', 'small', 'default']),
-        PropTypes.shape({
-            xs: PropTypes.number,
-            sm: PropTypes.number,
-            // Add other breakpoints if needed
-        }),
-    ]), // Since 4.8.0
+  /** The size of the avatar (number, 'large', 'small', 'default', or an object for responsive sizes) */
+  size: PropTypes.oneOf(['small', 'middle', 'large']),
 
-    /** The shape of the avatar ('circle' or 'square') */
-    shape: PropTypes.oneOf(['circle', 'square']), // Since 5.8.0
+  /** The shape of the avatar ('circle' or 'square') */
+  shape: PropTypes.oneOf(['circle', 'square']), // Since 5.8.0
+}
 
-};
+Group.defaultProps = {}
 
-Group.defaultProps = {};
-
-export default Group;
+export default Group
