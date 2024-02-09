49b8b82076dce7a3f55a885674035e7490afb436
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 11:25:01 -0700

    Added props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,101 +1,102 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { Input as ANTComponent } from "antd";
-import TextArea from "./TextArea/TextArea";
-import Password from "./Password/Password";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Input as ANTComponent } from 'antd'
+import TextArea from './TextArea/TextArea'
+import Password from './Password/Password'
 
 /**
  * @uxpindocurl https://ant.design/components/Input/
- * @uxpindescription A basic widget for getting the user input is a text field. 
+ * @uxpindescription A basic widget for getting the user input is a text field.
  */
 
 const Input = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent {...props} />
+  )
+}
 
 Input.propTypes = {
+  /** The label text displayed after (on the right side of) the input field */
+  addonAfter: PropTypes.node,
 
-    /** The label text displayed after (on the right side of) the input field */
-    addonAfter: PropTypes.node,
+  /** The label text displayed before (on the left side of) the input field */
+  addonBefore: PropTypes.node,
 
-    /** The label text displayed before (on the left side of) the input field */
-    addonBefore: PropTypes.node,
+  /** If allow to remove input content with clear icon */
+  allowClear: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({ clearIcon: PropTypes.node })]),
 
-    /** If allow to remove input content with clear icon */
-    allowClear: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.shape({ clearIcon: PropTypes.node })
-    ]),
+  /** Semantic DOM class */
+  classNames: PropTypes.objectOf(PropTypes.string),
 
-    /** Semantic DOM class */
-    classNames: PropTypes.objectOf(PropTypes.string),
+  /** Character count config */
+  count: PropTypes.object, // CountConfig
 
-    /** Character count config */
-    count: PropTypes.object, // CountConfig
+  /** The initial input content */
+  defaultValue: PropTypes.string,
 
-    /** The initial input content */
-    defaultValue: PropTypes.string,
+  /** Whether the input is disabled */
+  disabled: PropTypes.bool,
 
-    /** Whether the input is disabled */
-    disabled: PropTypes.bool,
+  /** The ID for input */
+  id: PropTypes.string,
 
-    /** The ID for input */
-    id: PropTypes.string,
+  /** The maximum number of characters in Input */
+  maxLength: PropTypes.number,
 
-    /** The maximum number of characters in Input */
-    maxLength: PropTypes.number,
+  /** Placeholder text for the Input */
+  placeholder: PropTypes.string,
 
-    /** The prefix icon for the Input */
-    prefix: PropTypes.node,
+  /** The prefix icon for the Input */
+  prefix: PropTypes.node,
 
-    /** Whether to show character count */
-    showCount: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.shape({
-            formatter: PropTypes.func // (info: { value: string, count: number, maxLength?: number }) => ReactNode
-        })
-    ]),
+  /** Read only setting for the Input */
+  readOnly: PropTypes.bool,
 
-    /** Set validation status */
-    status: PropTypes.oneOf(['error', 'warning']),
+  /** Whether to show character count */
+  showCount: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.shape({
+      formatter: PropTypes.func, // (info: { value: string, count: number, maxLength?: number }) => ReactNode
+    }),
+  ]),
 
-    /** Semantic DOM style */
-    styles: PropTypes.objectOf(PropTypes.object), // CSSProperties
+  /** Set validation status */
+  status: PropTypes.oneOf(['error', 'warning']),
 
-    /** The size of the input box */
-    size: PropTypes.oneOf(['large', 'middle', 'small']),
+  /** Semantic DOM style */
+  styles: PropTypes.objectOf(PropTypes.object), // CSSProperties
 
-    /** The suffix icon for the Input */
-    suffix: PropTypes.node,
+  /** The size of the input box */
+  size: PropTypes.oneOf(['large', 'middle', 'small']),
 
-    /**
-* Type of the `input` element. 
-*/
-    type: PropTypes.oneOf(['text', 'password', 'email', 'number', 'date']),
+  /** The suffix icon for the Input */
+  suffix: PropTypes.node,
 
-    /** The input content value */
-    value: PropTypes.string,
+  /**
+   * Type of the `input` element.
+   */
+  type: PropTypes.oneOf(['text', 'password', 'email', 'number', 'date']),
 
-    /** Variants of Input */
-    variant: PropTypes.oneOf(['outlined', 'borderless', 'filled']),
+  /** The input content value */
+  value: PropTypes.string,
 
-    /** Callback when user input */
-    onChange: PropTypes.func,
+  /** Variants of Input */
+  variant: PropTypes.oneOf(['outlined', 'borderless', 'filled']),
 
-    /** The callback function that is triggered when Enter key is pressed */
-    onPressEnter: PropTypes.func,
-};
+  /** Callback when user input */
+  onChange: PropTypes.func,
 
-Input.defaultProps = {};
+  /** The callback function that is triggered when Enter key is pressed */
+  onPressEnter: PropTypes.func,
+}
 
-Input.TextArea = TextArea;
+Input.defaultProps = {}
 
-Input.Password = Password;
+Input.TextArea = TextArea
 
-export default Input;
+Input.Password = Password
+
+export default Input
