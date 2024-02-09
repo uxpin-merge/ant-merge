ab452fc140c467e30505ee08330a5165dc6f5487
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 16:16:50 -0700

    cleanup

--- src/components/List/Presets/0-default.jsx
+++ src/components/List/Presets/0-default.jsx
@@ -1,18 +1,10 @@
-import * as React from 'react';
-import List from '../List';
-import Item from '../Item/Item';
+import * as React from 'react'
+import List from '../List'
+import Item from '../Item/Item'
 
 export default (
-
   <List uxpId="list-1">
-    {/* <Item uxpId="list-item-1">
-      test
-    </Item>
-    <Item uxpId="list-item-2">
-      test 2
-    </Item> */}
-    <Item uxpId="list-item-2">hello</Item>
-    <Item uxpId="list-item-22">hello2</Item>
+    <Item uxpId="list-item-2">List Item 1</Item>
+    <Item uxpId="list-item-22">List Item 1</Item>
   </List>
-
-);
+)
