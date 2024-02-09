88333388837368af7d6c836ada2159c33c42e362
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 11:06:53 -0700

    Cleanup

--- src/components/Form/Presets/0-default.jsx
+++ src/components/Form/Presets/0-default.jsx
@@ -1,42 +1,36 @@
-import * as React from 'react';
-import Form from '../Form';
-import Item from '../Item/Item';
-import Input from '../../Input/Input';
-import Button from '../../Button/Button';
+import * as React from 'react'
+import Form from '../Form'
+import Item from '../Item/Item'
+import Input from '../../Input/Input'
+import Button from '../../Button/Button'
+import Checkbox from '../../Checkbox/Checkbox'
+import Password from '../../Input/Password/Password'
 
 export default (
-  <Form
-    uxpId="form-1"
-  >
-    <Item uxpId="item-1" label="Username"
+  <Form uxpId="form-1">
+    <Item
+      uxpId="form-item-1"
+      label="Username"
       name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
+      rules={[{ required: true, message: 'Please input your username!' }]}
     >
       <Input uxpId="input-1" />
     </Item>
     <Item
-      uxpId="item-2"
+      uxpId="form-item-2"
       label="Password"
       name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    ><Input uxpId="input-2" /></Item>
-    <Item
-      uxpId="item-3"
+      rules={[{ required: true, message: 'Please input your password!' }]}
     >
-      <Button type="primary" htmlType="submit" uxpId="button-1">
+      <Password uxpId="input-2" placeholder="Enter password" visibilityToggle={true} />
+    </Item>
+    <Item uxpId="form-item-3" name="password">
+      <Checkbox uxpId="form-checkbox-1">Remember me</Checkbox>
+    </Item>
+    <Item uxpId="form-item-4">
+      <Button type="primary" htmlType="submit" uxpId="form-button-1">
         Submit
       </Button>
     </Item>
-
   </Form>
-);
+)
