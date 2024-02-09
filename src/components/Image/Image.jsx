68469a677a2536e5ec059fddb875c20100c0f792
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-09 16:10:04 -0700

    Prop update

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { Image as ANTComponent } from "antd";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Image as ANTComponent } from 'antd'
 
 /**
  * @uxpindocurl https://ant.design/components/Image/
@@ -8,40 +8,38 @@ import { Image as ANTComponent } from "antd";
  */
 
 const Image = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent {...props} />
+  )
+}
 
 Image.propTypes = {
-    /** The content of the component. */
-    // children: PropTypes.node,
+  /** The content of the component. */
+  // children: PropTypes.node,
 
-    /** The style properties of the component */
-    style: PropTypes.object,
+  /** The style properties of the component */
+  style: PropTypes.object,
 
-    /** Image description */ alt: PropTypes.string,
+  /** Image description */ alt: PropTypes.string,
 
- /** Load failure fault-tolerant src */ fallback: PropTypes.string,
+  /** Load failure fault-tolerant src */ fallback: PropTypes.string,
 
- /** Image height */ height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Image height */ height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
- /** Load placeholder, use default placeholder when set true */ placeholder: PropTypes.node,
+  /** Load placeholder, use default placeholder when set true */ placeholder: PropTypes.node,
 
- /** Preview config, disabled when false */ preview: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
+  /** Preview config, disabled when false */ preview: PropTypes.bool,
 
- /** Image path */ src: PropTypes.string,
+  /** Image path */ src: PropTypes.string,
 
- /** Image width */ width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Image width */ width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
- /** Load failed callback */ onError: PropTypes.func,
+  /** Load failed callback */ onError: PropTypes.func,
+}
 
-};
+Image.defaultProps = {}
 
-Image.defaultProps = {};
-
-export default Image;
+export default Image
