05aebd32a413155914367fa6038f14404d1625ac
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-13 14:33:00 -0700

    cleanup

--- src/components/Watermark/Watermark.jsx
+++ src/components/Watermark/Watermark.jsx
@@ -12,7 +12,7 @@ const Watermark = (props) => {
 
   return (
     // Your component JSX
-    <ANTComponent {...props} style={{ minWidth: '300px', minHeight: '300px', width: '100%', height: '100%' }} />
+    <ANTComponent style={{ width: '100%', height: '100%' }} {...props} />
   )
 }
 
@@ -26,24 +26,34 @@ Watermark.propTypes = {
   /** Watermark text content
    */
   content: PropTypes.arrayOf(PropTypes.string),
+
   /** Watermark text style */
-  color: PropTypes.string /** Font size */,
-  fontSize: PropTypes.number /** Font weight */,
-  fontWeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]) /** Font family */,
-  fontFamily: PropTypes.string /** Font style */,
-  fontStyle: PropTypes.oneOf(['none', 'normal', 'italic', 'oblique']) /** Text alignment direction */,
-  textAlign: PropTypes.oneOf(['start', 'end', 'left', 'right', 'center']) /** The spacing between watermarks */,
-  gap: PropTypes.arrayOf(
-    PropTypes.number
-  ) /** The height of the watermark, the default value of content is its own height */,
-  height: PropTypes.number /** Pass the watermark to the pop-up component such as Modal, Drawer */,
-  inherit:
-    PropTypes.bool /** Image source, it is recommended to export 2x or 3x image, high priority (support base64 format) */,
-  image:
-    PropTypes.string /** The offset of the watermark from the upper left corner of the container. The default is gap/2 */,
-  offset: PropTypes.arrayOf(PropTypes.number) /** The rotation Angle when the watermark is drawn, unit ° */,
-  rotate: PropTypes.number /** The width of the watermark, the default value of content is its own width */,
-  width: PropTypes.number /** The z-index of the appended watermark element */,
+  color: PropTypes.string,
+  /** Font size */
+  fontSize: PropTypes.number,
+  /** Font weight */
+  fontWeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Font family */
+  fontFamily: PropTypes.string,
+  /** Font style */
+  fontStyle: PropTypes.oneOf(['none', 'normal', 'italic', 'oblique']),
+  /** Text alignment direction */
+  textAlign: PropTypes.oneOf(['start', 'end', 'left', 'right', 'center']),
+  /** The spacing between watermarks */
+  gap: PropTypes.arrayOf(PropTypes.number),
+  /** The height of the watermark, the default value of content is its own height */
+  height: PropTypes.number,
+  /** Pass the watermark to the pop-up component such as Modal, Drawer */
+  inherit: PropTypes.bool,
+  /** Image source, it is recommended to export 2x or 3x image, high priority (support base64 format) */
+  image: PropTypes.string,
+  /** The offset of the watermark from the upper left corner of the container. The default is gap/2 */
+  offset: PropTypes.arrayOf(PropTypes.number),
+  /** The rotation Angle when the watermark is drawn, unit ° */
+  rotate: PropTypes.number,
+  /** The width of the watermark, the default value of content is its own width */
+  width: PropTypes.number,
+  /** The z-index of the appended watermark element */
   zIndex: PropTypes.number,
 }
 
