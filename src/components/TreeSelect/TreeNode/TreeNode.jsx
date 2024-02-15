2561771946d2589078de569b5a745502ca4ff77b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-15 12:07:42 -0700

    Added Icon prop

--- src/components/TreeSelect/TreeNode/TreeNode.jsx
+++ src/components/TreeSelect/TreeNode/TreeNode.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { TreeSelect as ANTComponent } from "antd";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TreeSelect as ANTComponent } from 'antd'
 
 /**
  * @uxpindocurl https://ant.design/components/tree-select/
@@ -9,47 +9,49 @@ import { TreeSelect as ANTComponent } from "antd";
  */
 
 const TreeNode = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent.TreeNode {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent.TreeNode {...props} />
+  )
+}
 
 TreeNode.propTypes = {
-    /** The content of the component. */
-    children: PropTypes.node,
+  /** The content of the component. */
+  children: PropTypes.node,
 
-    /** The style properties of the component */
-    style: PropTypes.object,
+  /** The style properties of the component */
+  style: PropTypes.object,
 
-    /** When Tree is checkable, set TreeNode display Checkbox or not */
-    checkable: PropTypes.bool,
+  /** When Tree is checkable, set TreeNode display Checkbox or not */
+  checkable: PropTypes.bool,
 
-    /** Disables the checkbox of the treeNode */
-    disableCheckbox: PropTypes.bool,
+  /** Disables the checkbox of the treeNode */
+  disableCheckbox: PropTypes.bool,
 
-    /** Disabled or not */
-    disabled: PropTypes.bool,
+  /** Disabled or not */
+  disabled: PropTypes.bool,
 
-    /** Leaf node or not */
-    isLeaf: PropTypes.bool,
+  /** Trenode Icon */
+  icon: PropTypes.node,
 
-    /** Required property (unless using treeDataSimpleMode), should be unique in the tree */
-    key: PropTypes.string,
+  /** Leaf node or not */
+  isLeaf: PropTypes.bool,
 
-    /** Whether can be selected */
-    selectable: PropTypes.bool,
+  /** Required property (unless using treeDataSimpleMode), should be unique in the tree */
+  key: PropTypes.string,
 
-    /** Content showed on the treeNodes */
-    title: PropTypes.node,
+  /** Whether can be selected */
+  selectable: PropTypes.bool,
 
-    /** Will be treated as treeNodeFilterProp by default, should be unique in the tree */
-    value: PropTypes.string,
-};
+  /** Content showed on the treeNodes */
+  title: PropTypes.node,
 
-TreeNode.defaultProps = {};
+  /** Will be treated as treeNodeFilterProp by default, should be unique in the tree */
+  value: PropTypes.string,
+}
 
-export default TreeNode;
+TreeNode.defaultProps = {}
+
+export default TreeNode
