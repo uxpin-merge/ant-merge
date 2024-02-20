3955e0541e72419853d0701e678996ad196b7616
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-19 20:07:26 -0700

    @uxpinbind

--- src/components/Layout/Sider/Sider.jsx
+++ src/components/Layout/Sider/Sider.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { Layout as ANTComponent } from "antd";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Layout as ANTComponent } from 'antd'
 
 /**
  * @uxpindocurl https://ant.design/components/Layout/
@@ -9,62 +9,60 @@ import { Layout as ANTComponent } from "antd";
  */
 
 const Sider = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent.Sider {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent.Sider {...props} />
+  )
+}
 
 Sider.propTypes = {
-    /** The content of the component. */
-    children: PropTypes.node,
+  /** Children components inside the Sider */
+  children: PropTypes.node,
 
-    /** Children components inside the Sider */
-    children: PropTypes.node,
+  /** The breakpoint at which the Sider will automatically collapse */
+  breakpoint: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
 
-    /** The breakpoint at which the Sider will automatically collapse */
-    breakpoint: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  /** Whether the Sider is collapsible  */
+  collapsible: PropTypes.bool,
 
-    /** Whether the Sider is collapsible */
-    collapsible: PropTypes.bool,
+  /** Current collapse status of the Sider
+   *  @uxpinbind onCollapse
+   */
+  collapsed: PropTypes.bool,
 
-    /** Current collapse status of the Sider */
-    collapsed: PropTypes.bool,
+  /** Width of the Sider when it's collapsed */
+  collapsedWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
-    /** Width of the Sider when it's collapsed */
-    collapsedWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /** Custom class name for the sider */
+  className: PropTypes.string,
 
-    /** Custom class name for the sider */
-    className: PropTypes.string,
+  /** To set the default collapsed status */
+  defaultCollapsed: PropTypes.bool,
 
-    /** To set the default collapsed status */
-    defaultCollapsed: PropTypes.bool,
+  /** Triggered when the collapse status is changed */
+  onCollapse: PropTypes.func,
 
-    /** Triggered when the collapse status is changed */
-    onCollapse: PropTypes.func,
+  /** Reverse direction of arrow, for a collapsible sider */
+  reverseArrow: PropTypes.bool,
 
-    /** Reverse direction of arrow, for a collapsible sider */
-    reverseArrow: PropTypes.bool,
+  /** Custom style for the sider */
+  style: PropTypes.object,
 
-    /** Custom style for the sider */
-    style: PropTypes.object,
+  /** The theme of the Sider */
+  theme: PropTypes.oneOf(['light', 'dark']),
 
-    /** The theme of the Sider */
-    theme: PropTypes.oneOf(['light', 'dark']),
+  /** Whether to render the trigger or not */
+  trigger: PropTypes.node,
 
-    /** Whether to render the trigger or not */
-    trigger: PropTypes.node,
+  /** Width of the Sider when it's unfolded */
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
-    /** Width of the Sider when it's unfolded */
-    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /** Zero width trigger styling */
+  zeroWidthTriggerStyle: PropTypes.object,
+}
 
-    /** Zero width trigger styling */
-    zeroWidthTriggerStyle: PropTypes.object,
-};
+Sider.defaultProps = {}
 
-Sider.defaultProps = {};
-
-export default Sider;
+export default Sider
