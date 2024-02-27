88a203f45707bf3de251a61d267fb4fe1b692365
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-27 13:05:30 -0700

    test thumbnail

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import Button from '../Button';
+import * as React from 'react'
+import Button from '../Button'
 
 export default (
-  <Button uxpId="button-1">
-    Default Button
-  </Button>
-);
+  <>
+    <img src="https://ant.design/components/overview/https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*7va7RKs3YzIAAAAAAAAAAAAADrJ8AQ/original" />
+    <Button uxpId="button-1">Default Button</Button>
+  </>
+)
