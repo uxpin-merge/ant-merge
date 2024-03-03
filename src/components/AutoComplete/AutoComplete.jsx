15f8dc7e4bf9aa1c84c2cf275f6c4b62b083075e
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-03-02 18:40:24 -0700

    Update prop and value binding

--- src/components/AutoComplete/AutoComplete.jsx
+++ src/components/AutoComplete/AutoComplete.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { AutoComplete as ANTComponent } from "antd";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AutoComplete as ANTComponent } from 'antd'
 
 /**
  * @uxpindocurl https://ant.design/components/AutoComplete/
@@ -8,57 +8,100 @@ import { AutoComplete as ANTComponent } from "antd";
  */
 
 const AutoComplete = (props) => {
-    // Component logic
+  // Component logic
 
-    return (
-        // Your component JSX
-        <ANTComponent {...props} />
-
-    );
-};
+  return (
+    // Your component JSX
+    <ANTComponent {...props} style={{ width: '100%' }} />
+  )
+}
 
 AutoComplete.propTypes = {
-    /** The content of the component. */
-    children: PropTypes.node,
+  /** The content of the component. */
+  children: PropTypes.node,
 
-    /** The style properties of the component */
-    style: PropTypes.object,
+  /** The style properties of the component */
+  style: PropTypes.object,
 
-    /** Children components, typically `AutoComplete.Option` */
-    children: PropTypes.node,
-    /** Additional CSS class for the AutoComplete component */
-    className: PropTypes.string,
-    /** The default value of the input */
-    defaultValue: PropTypes.string,
-    /** Whether the AutoComplete is disabled */
-    disabled: PropTypes.bool,
-    /** The data source for autocomplete suggestions */
-    dataSource: PropTypes.arrayOf(PropTypes.string),
-    /** Function that will be called when a selection is made */
-    onSelect: PropTypes.func,
-    /** Function that will be called when the input is changed */
-    onChange: PropTypes.func,
-    /** Placeholder text for the input */
-    placeholder: PropTypes.string,
-    /** The value of the input
-     * @uxpinbind onChange 
-    */
-    value: PropTypes.string,
-    /** Style object for the container of the AutoComplete */
-    style: PropTypes.object,
-    /** The maximum height of the dropdown menu in pixels */
-    dropdownMatchSelectWidth: PropTypes.number,
-    /** Whether to show a loading indicator */
-    loading: PropTypes.bool,
-    /** Custom render function for the input element */
-    renderInput: PropTypes.func,
-    /** Custom render function for the dropdown menu */
-    renderOptions: PropTypes.func,
-    /** Whether to allow clearing the input */
-    allowClear: PropTypes.bool,
+  /** Show clear button */
+  allowClear: PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({ clearIcon: PropTypes.node })]),
 
-};
+  /** If get focus when component mounted */
+  autoFocus: PropTypes.bool,
 
-AutoComplete.defaultProps = {};
+  /** If backfill selected item the input when using keyboard */
+  backfill: PropTypes.bool,
 
-export default AutoComplete;
+  /** Data source to auto complete */
+  dataSource: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+
+  /** Whether active first option by default */
+  defaultActiveFirstOption: PropTypes.bool,
+
+  /** Initial open state of dropdown */
+  defaultOpen: PropTypes.bool,
+
+  /** Initial selected option */
+  defaultValue: PropTypes.string,
+
+  /** Whether disabled select */
+  disabled: PropTypes.bool,
+
+  /** The className of dropdown menu */
+  popupClassName: PropTypes.string,
+
+  /** Determine whether the dropdown menu and the select input are the same width */
+  dropdownMatchSelectWidth: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+
+  /** If true, filter options by input, if function, filter options against it */
+  filterOption: PropTypes.bool,
+
+  /** Specify content to show when no result matches */
+  notFoundContent: PropTypes.node,
+
+  /** Controlled open state of dropdown */
+  open: PropTypes.bool,
+
+  /** Select options
+   */
+  options: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.node, value: PropTypes.string })),
+
+  /** The placeholder of input */
+  placeholder: PropTypes.string,
+
+  /** Set validation status */
+  status: PropTypes.oneOf(['error', 'warning']),
+
+  /** Selected option
+   * @uxpinbind onChange
+   */
+  value: PropTypes.string,
+
+  /** Variants of input */
+  variant: PropTypes.oneOf(['outlined', 'borderless', 'filled']),
+
+  /** Called when leaving the component */
+  onBlur: PropTypes.func,
+
+  /** Called when selecting an option or changing an input value */
+  onChange: PropTypes.func,
+
+  /** Call when dropdown open */
+  onDropdownVisibleChange: PropTypes.func,
+
+  /** Called when entering the component */
+  onFocus: PropTypes.func,
+
+  /** Called when searching items */
+  onSearch: PropTypes.func,
+
+  /** Called when a option is selected. param is option's value and option instance */
+  onSelect: PropTypes.func,
+
+  /** Called when clear */
+  onClear: PropTypes.func,
+}
+
+AutoComplete.defaultProps = {}
+
+export default AutoComplete
