1c6c8129d285d18e3c6652e2b1066e6ca01c4b47
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-09-27 18:57:25 -0600

    Theme examples and globall css file

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,66 +1,88 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import defaultTheme from './boilerplate-theme';
+import React from 'react'
+import defaultTheme from './boilerplate-theme'
 
-import { ConfigProvider } from 'antd';
+import { ConfigProvider } from 'antd'
 // import { StyleProvider } from '@ant-design/cssinjs';
 
-// import './index.css'
+import './index.css'
 
+export const ThemeContext = React.createContext({})
+const completeDefaultTheme = defaultTheme
 
-export const ThemeContext = React.createContext({});
-const completeDefaultTheme = defaultTheme;
-
-let listeners = [];
+let listeners = []
 
 const addListener = (listener) => {
-  listeners.push(listener);
-};
+  listeners.push(listener)
+}
 
 const removeListener = (listener) => {
-  listeners = listeners.filter((lis) => lis !== listener);
-};
+  listeners = listeners.filter((lis) => lis !== listener)
+}
 
 //global theme object for all wrapper instances
 let themeOptions = {
   theme: completeDefaultTheme,
-};
+}
 
 //allows to update the global theme object and updates all wrapper instances with the new theme
 //expects a function as a parameter, which itself expects the old theme as a parameter
 const setThemeOptions = (callback) => {
   //the callback function is called and given the old theme as a parameter
   //the result of the callback function is saved in themeOptions (the new theme)
-  themeOptions = callback(themeOptions);
-  console.log('theme was updated');
+  themeOptions = callback(themeOptions)
+  console.log('theme was updated')
   //each listener is a function which sets the theme in the state of its UXPinWrapper component to a given theme
   //each function (listener) is executed with the given theme as a parameter, so all UXPinWrapper instances are updated with the same theme
   listeners.forEach((listener) => {
-    listener(themeOptions);
-  });
-};
+    listener(themeOptions)
+  })
+}
 
 export default function UXPinWrapper(props) {
-  const [theme, setTheme] = React.useState(themeOptions);
+  //Link Google fonts
+  if (!document.getElementById('fluent-merge-font-DmSans')) {
+    let DmSans = document.createElement('link')
+    DmSans.setAttribute(
+      'href',
+      'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap'
+    )
+    DmSans.setAttribute('rel', 'stylesheet')
+    DmSans.setAttribute('id', 'fluent-merge-font-DmSans')
+    document.head.appendChild(DmSans)
+  }
+
+  if (!document.getElementById('fluent-merge-font-SpaceGrotesk')) {
+    let SpaceGrotesk = document.createElement('link')
+    SpaceGrotesk.setAttribute(
+      'href',
+      'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap'
+    )
+    SpaceGrotesk.setAttribute('rel', 'stylesheet')
+    SpaceGrotesk.setAttribute('id', 'fluent-merge-font-SpaceGrotesk')
+    document.head.appendChild(SpaceGrotesk)
+  }
+
+  const [theme, setTheme] = React.useState(themeOptions)
 
   React.useEffect(() => {
     //updates the component state to a given theme
     const onThemeChange = (newTheme) => {
-      setTheme(newTheme);
-    };
-    console.log(theme);
+      setTheme(newTheme)
+    }
+    console.log(theme)
     //the component adds a function to the listener array, which allows to update its state with a given theme
-    addListener(onThemeChange);
+    addListener(onThemeChange)
     //the listener is removed when the component unmounts (is deleted from the canvas)
-    return () => removeListener(onThemeChange);
-  });
+    return () => removeListener(onThemeChange)
+  })
 
   return (
     <ThemeContext.Provider value={[theme, setThemeOptions]}>
-      <ConfigProvider  theme={{ hashed: false, ...themeOptions.theme }} {...props}>
+      <ConfigProvider theme={{ hashed: false, ...themeOptions.theme }} {...props}>
         {props.children}
       </ConfigProvider>
     </ThemeContext.Provider>
-  );
+  )
 }
--- src/components/UXPinWrapper/boilerplate-theme.ts
+++ src/components/UXPinWrapper/boilerplate-theme.ts
@@ -1,28 +1,8 @@
 
 const boilerplateTheme = {
-  // hashed: false,
   token: {
-    // "colorPrimary": "red",
-    // "colorSuccess": "#52c41a",
-    // "colorWarning": "#faad14",
-    // "colorError": "#ff4d4f",
-    // "colorInfo": "#1677ff",
-    // "colorLink": "",
-    // "colorTextBase": "",
-    // "colorBgBase": "",
-    // "fontFamily": "Helvetica Neue",
-    // "fontFamilyCode": "'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace",
-    // "fontSize": 14,
-    // "lineWidth": 1,
-    // "lineType": "solid",
-    // "borderRadius": 2,
-    // "sizeUnit": 4,
-    // "sizeStep": 4,
-    // "sizePopupArrow": 16,
-    // "controlHeight": 32,
-    // "zIndexBase": 0,
-    // "zIndexPopupBase": 1000,
-  }
+    fontFamily: 'DM Sans',
+  },
 };
 
 export default boilerplateTheme;
